ff733735703cfe650df95b4f23a89e3178492b69
Matt Hackmann <matt@dxprog.com>
2016-02-22 18:56:08 -0800

    build system up and running

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            options: {
+                transform: [
+                    [ 'babelify', { presets: 'es2015' } ]
+                ]
+            },
+            dist: {
+              files: {
+                  './dist/coolboy.js': [ './src/app.js' ]
+              }
+            }
+        },
+        watch: {
+            js: {
+                files: [
+                    './src/**/*.js'
+                ],
+                tasks: [ 'browserify' ],
+            },
+            configFiles: {
+                files: [ 'Gruntfile.js' ],
+                options: {
+                    reload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.registerTask('default', [ 'browserify' ]);
+};
