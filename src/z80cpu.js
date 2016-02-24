c4972d08ec1b112f6fc5eae45f3a606c0c7e6f4c
Matt Hackmann <matt@dxprog.com>
2016-02-23 21:10:49 -0800

    rework to better represent CPU internals

--- src/z80cpu.js
+++ src/z80cpu.js
@@ -1,14 +1,24 @@
 const REG_A = '_a';
 const REG_B = '_b';
 const REG_C = '_c';
-const REG_HL = '_hl';
+const REG_D = '_d';
+const REG_E = '_e';
+const REG_H = '_h';
+const REG_L = '_l';
+const REG_F = '_f';
 const FLAG_Z = 0x40;
 const FLAG_S = 0x80;
+
+const WORD_CLAMP = 0xffff;
+const BYTE_CLAMP = 0xff;
+const WORK_RAM_SIZE = 0x3fff;
+const WORK_RAM_START = 0xc000;
 
 const Cpu = function(rom, ppu) {
   // Pointers to other parts of the machine
   this._rom = rom;
   this._ppu = ppu;
+  this._wram = new Uint8Array(WORK_RAM_SIZE);
   
   // Reset the CPU's internal state
   this.reset();
@@ -18,13 +28,15 @@ Cpu.prototype = {
   reset() {
     this._sp = 0; // Stack pointer
     this._pc = 0; // Program counter
-    this._a = 0; // Accumulator
-    this._b = 0; // B
-    this._c = 0; // C
-    this._hl = 0; // HL (16-bit reg)
-    this._f = 0; // Status flags
+    this[REG_A] = 0; // Accumulator
+    this[REG_B] = 0; // B
+    this[REG_C] = 0; // C
+    this[REG_D] = 0; // D
+    this[REG_E] = 0; // E
+    this[REG_H] = 0; // H
+    this[REG_L] = 0; // L
+    this[REG_F] = 0; // Status flags
     this._di = false; // Disable interrupt
-    this._stack = [];
   },
   
   begin() {
@@ -53,6 +65,7 @@ Cpu.prototype = {
    * Reads value from memory mapped address
    * 
    * @method _readMem
+   * @private
    * @param {Uint16} addr The address to read
    * @return {Uint8} The value at the address
    */
@@ -60,10 +73,15 @@ Cpu.prototype = {
     let retVal = 0;
     
     // Memory map:
-    // ROM
-    addr &= 0xffff;
+    // ROM (0x0000 - 0x7fff)
+    addr &= WORD_CLAMP;
     if (addr < 0x8000) {
       retVal = this._rom.readUint8(addr);
+      
+    // WRAM (0xc000 - 0xffff)
+    } else if (addr >= WORK_RAM_START) {
+      retVal = this._wram[addr - WORK_RAM_START];
+
     } else {
       throw `Memory address space unimplemented: ${addr.toString(16)}`;
     }
@@ -71,8 +89,21 @@ Cpu.prototype = {
     return retVal;
   },
   
+  /**
+   * Writes a value to a memory mapped address
+   * 
+   * @method _writeMem
+   * @private
+   * @param {Uint16} add The address to write to
+   * @param {Uint8} value The value to write
+   * @return {Void}
+   */
   _writeMem(addr, value) {
-    console.log('write: ', addr.toString(16), value.toString(16));
+    value &= BYTE_CLAMP;
+    addr &= WORD_CLAMP;
+    if (addr >= WORK_RAM_START) {
+      this._wram[addr - WORK_RAM_START] = value;
+    }
   },
   
   // 16-bit PC read
@@ -140,13 +171,13 @@ Cpu.prototype = {
    * XOR
    */
   _xor(value) {
-    this._a = this._a ^ value;
+    this[REG_A] = this[REG_A] ^ value;
     this._cycles += 4;
-    this._setFlags(this._a);
+    this._setFlags(this[REG_A]);
   },
   
   opaf() {
-    this._xor(this._a);
+    this._xor(this[REG_A]);
   },
   
   /**
@@ -160,7 +191,8 @@ Cpu.prototype = {
   
   // LD hl,xx
   op21() {
-    this._hl = this._readOp16();
+    this[REG_L] = this._readOp();
+    this[REG_H] = this._readOp();
     this._cycles += 10;
   },
   
@@ -182,7 +214,8 @@ Cpu.prototype = {
   // LD (HLD),a
   // This is custom to the GameBoy. Writes A into (HL) and then decrements HL
   op32() {
-    this._writeMem(this._hl, this._a);
+    const addr = (this[REG_H] << 0x8) | this[REG_L];
+    this._writeMem(addr, this[REG_A]);
     this._cycles += 13;
     this.op2b(); // DEC hl
   },
@@ -191,13 +224,17 @@ Cpu.prototype = {
    * DEC
    */
   _decReg8(reg) {
-    this[reg] = (this[reg] - 1) & 0xff;
+    this[reg] = (this[reg] - 1) & BYTE_CLAMP;
     this._cycles += 4;
     this._setFlags(this[reg]);
   },
   
-  _decReg16(reg) {
-    this[reg] = (this[reg] - 1) & 0xffff;
+  // Helper function because fake 16-bit registers are dumb
+  _decReg16(reg1, reg2) {
+    let value = (this[reg1] << 0x8) | this[reg2];
+    value = (value - 1) & WORD_CLAMP;
+    this[reg1] = value >> 0x8;
+    this[reg2] = value & BYTE_CLAMP;
     this._cycles += 6;
   },
   
@@ -213,7 +250,7 @@ Cpu.prototype = {
   
   // DEC hl
   op2b() {
-    this._decReg16(REG_HL);
+    this._decReg16(REG_H, REG_L);
   },
   
   /**
@@ -239,7 +276,7 @@ Cpu.prototype = {
    */
   opf3() {
     this._di = true;
-    op0();
+    this.op0();
   },
   
   opfb() {
@@ -250,9 +287,8 @@ Cpu.prototype = {
    * PUSH
    */  
   _push(value) {
-    this._stack.push(value & 0xff);
-    this._stack.push(value >> 0x8);
-    this._sp++;
+    this._writeMem(this._sp++, value & BYTE_CLAMP);
+    this._writeMem(this._sp++, value >> 8);
     this._cycles += 11;
   },
   
@@ -260,8 +296,8 @@ Cpu.prototype = {
    * POP
    */
   _pop() {
-    const msb = this._stack.pop();
-    return (msb << 0x8) | this._stack.pop();
+    const msb = this._readMem(this._sp--);
+    return (msb << 0x8) | this._readMem(this._sp--);
   },
   
   /**
