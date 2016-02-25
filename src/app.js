3983916635fb556608bbdb28418ee917eae47825
Matt Hackmann <matt@dxprog.com>
2016-02-24 20:16:46 -0800

    whitespace + opcode cleanup

--- src/app.js
+++ src/app.js
@@ -4,14 +4,8 @@ import Z80 from './z80cpu';
 const romLoader = new RomLoader();
 romLoader.load('tetris').then(() => {
   const cpu = new Z80(romLoader, null);
-  let ticks = 1000;
   cpu.begin();
   while (true) {
     cpu.tick();
-    if (ticks) {
-      ticks--;
-    } else {
-      break;
-    }
   }
 });
--- src/z80cpu.js
+++ src/z80cpu.js
@@ -21,11 +21,11 @@ const Cpu = function(rom, ppu) {
   // Pointers to other parts of the machine
   this._rom = rom;
   this._ppu = ppu;
-  
+
   // Initialize work RAM here. A CPU reset will _NOT_ clear its contents
   this._wram = new Uint8Array(WORK_RAM_SIZE);
   this._wram.forEach((value, index) => this._wram[index] = 0);
-  
+
   // Reset the CPU's internal state
   this.reset();
 };
@@ -44,15 +44,15 @@ Cpu.prototype = {
     this[REG_F] = 0; // Status flags
     this._di = false; // Disable interrupt
   },
-  
+
   begin() {
     this.reset();
     this._pc = this._rom.getStartVector();
-    
+
     // Cycles is the number of cycles required to perform a particular operation
     this._cycles = 0;
   },
-  
+
   tick() {
     if (this._cycles <= 0) {
       this._currentAddr = this._pc.toString(16);
@@ -67,15 +67,15 @@ Cpu.prototype = {
       this._cycles--;
     }
   },
-  
+
   _log(text) {
-    const addr = '0'.repeat(4 - this._currentAddr.length) + this._currentAddr; 
+    const addr = '0'.repeat(4 - this._currentAddr.length) + this._currentAddr;
     console.log(`${addr}: ${text}`);
   },
-  
+
   /**
    * Reads value from memory mapped address
-   * 
+   *
    * @method _readMem
    * @private
    * @param {Uint16} addr The address to read
@@ -83,13 +83,13 @@ Cpu.prototype = {
    */
   _readMem(addr) {
     let retVal = 0;
-    
+
     // Memory map:
     // ROM (0x0000 - 0x7fff)
     addr &= WORD_CLAMP;
     if (addr < 0x8000) {
       retVal = this._rom.readUint8(addr);
-      
+
     // WRAM (0xc000 - 0xffff)
     } else if (addr >= WORK_RAM_START) {
       retVal = this._wram[addr - WORK_RAM_START];
@@ -100,10 +100,10 @@ Cpu.prototype = {
     // console.log('read: ', addr.toString(16), retVal.toString(16));
     return retVal;
   },
-  
+
   /**
    * Writes a value to a memory mapped address
-   * 
+   *
    * @method _writeMem
    * @private
    * @param {Uint16} add The address to write to
@@ -117,30 +117,30 @@ Cpu.prototype = {
       this._wram[addr - WORK_RAM_START] = value;
     }
   },
-  
+
   // 16-bit PC read
   _readOp16(addr) {
     const lsb = this._readOp();
     return (this._readOp() << 0x8) | lsb;
   },
-  
+
   // 8-bit PC read
   _readOp() {
     this._cycles++;
     return this._readMem(this._pc++);
   },
-  
+
   /****************************************
    *                OP CODES              *
    ***************************************/
-  
+
   /**
    * Given a value, sets the appropriate flag
    */
   _setFlags(value) {
     this._setFlag(FLAG_Z, value === 0);
   },
-  
+
   /**
    * Sets an individual flag
    */
@@ -148,26 +148,26 @@ Cpu.prototype = {
     this[REG_F] = high ? this[REG_F] |= flag : this[REG_F] &= ~flag;
     // console.log('reg: ', flag.toString(16), high, this._f);
   },
-  
+
   /**
    * Returns a flag's status as a boolean
    */
   _getFlag(flag) {
     return !!(this._f & flag);
   },
-  
+
   // NOP
   op0() {
     this._log('nop');
     this._cycles += 4;
   },
-  
+
   // Normally this would be: EX (sp),hl
   // But not for Nintendo...
   ope3() {
     this.op0();
   },
-  
+
   /**
    * JP
    */
@@ -176,12 +176,12 @@ Cpu.prototype = {
     this._pc = addr;
     this._cycles += 10;
   },
-  
+
   // JP xx
   opc3() {
     this._jp(this._readOp16());
   },
-  
+
   /**
    * XOR
    */
@@ -191,15 +191,15 @@ Cpu.prototype = {
     this._cycles += 4;
     this._setFlags(this[REG_A]);
   },
-  
+
   opaf() {
     this._xor(this[REG_A]);
   },
-  
+
   /**
    * LD
    */
-  
+
   // Loads a value into a register
   _ldReg8(reg, value) {
     this._log(`ld ${reg}, 0x${value.toString(16)}`);
@@ -207,14 +207,14 @@ Cpu.prototype = {
     this._cycles += 4;
     this._setFlags(value);
   },
-  
+
   // Loads the value of one register into another
   _ldRegWithReg(regTo, regFrom) {
     this._log(`ld ${regTo}, ${regFrom}`);
     this[regTo] = this[regFrom];
     this._cycles += 4;
   },
-  
+
   // Loads the value of the accumulator into the memory address
   // of a 16-bit register combo
   _ldIndirectA(reg1, reg2) {
@@ -223,7 +223,7 @@ Cpu.prototype = {
     this._writeMem(addr, this[REG_A]);
     this._cycles += 8;
   },
-  
+
   // LD hl,xx
   op21() {
     this[REG_L] = this._readOp();
@@ -231,7 +231,7 @@ Cpu.prototype = {
     this._log(`ld hl, 0x${((this[REG_H] << 8) | this[REG_L]).toString(16)}`);
     this._cycles += 10;
   },
-  
+
   // LD a, a
   op7f() {
     this._ldRegWithReg(REG_A, REG_A);
@@ -271,22 +271,22 @@ Cpu.prototype = {
   ope() {
     this._ldReg8(REG_C, this._readOp());
   },
-  
+
   // LD b,x
   op6() {
     this._ldReg8(REG_B, this._readOp());
   },
-  
+
   // LD a,x
   op3e() {
     this._ldReg8(REG_A, this._readOp());
   },
-  
+
   // LD a, (xx)
   opfa() {
     this._ldReg8(REG_A, this._readMem(this._readOp16()));
   },
-  
+
   // LD (bc), a
   op2() {
     this._ldIndirectA(REG_B, REG_C);
@@ -311,7 +311,7 @@ Cpu.prototype = {
     this._cycles += 13;
     this.op2b(); // DEC hl
   },
-  
+
   /**
    * DEC
    */
@@ -322,7 +322,7 @@ Cpu.prototype = {
     this._setFlags(this[reg]);
     this._setFlag(FLAG_N, true);
   },
-  
+
   // Helper function because fake 16-bit registers are dumb
   _decReg16(reg1, reg2) {
     this._log(`dec ${reg1}${reg2}`);
@@ -333,42 +333,42 @@ Cpu.prototype = {
     this._cycles += 6;
     this._setFlag(FLAG_N, true);
   },
-  
+
   // DEC b
   op5() {
     this._decReg8(REG_B);
   },
-  
+
   // DEC c
   opd() {
     this._decReg8(REG_C);
   },
-  
+
   // DEC hl
   op2b() {
     this._decReg16(REG_H, REG_L);
   },
-  
+
   /**
    * JR (jump relative)
    */
   _jr(cond, addr) {
     if (cond) {
       // Calculate up the direction
-      addr = !!(addr & 0x80) ? -((~addr & BYTE_CLAMP) + 1) : addr & 0x7f;
-      this._pc = (this._pc + addr) & 0xffff;
+      const offset = !!(addr & 0x80) ? -((~addr & BYTE_CLAMP) + 1) : addr & 0x7f;
+      this._pc = (this._pc + offset) & 0xffff;
       this._cycles += 5;
     }
     this._cycles += 7;
   },
-  
+
   // JR NZ,x
   op20() {
     const op = this._readOp();
-    this._log(`jp nz, 0x${op.toString(16)}`);
+    this._log(`jr nz, 0x${op.toString(16)}`);
     this._jr(!this._getFlag(FLAG_Z), op);
   },
-  
+
   /**
    * Interrupts
    */
@@ -377,21 +377,21 @@ Cpu.prototype = {
     this._di = true;
     this.op0();
   },
-  
+
   opfb() {
     this._log('enable interrupts');
     this._di = false;
   },
-  
+
   /**
    * PUSH
-   */  
+   */
   _push(value) {
     this._writeMem(this._sp--, value & BYTE_CLAMP);
     this._writeMem(this._sp--, value >> 0x8);
     this._cycles += 11;
   },
-  
+
   /**
    * POP
    */
@@ -401,7 +401,7 @@ Cpu.prototype = {
     this._log('pop', addr.toString(16));
     return addr;
   },
-  
+
   /**
    * CALL
    */
@@ -411,12 +411,12 @@ Cpu.prototype = {
     this._pc = addr;
     this._cycles += 6;
   },
-  
+
   // CALL xx
   opcd() {
     this._call(this._readOp16());
   },
-  
+
   /**
    * RET
    */
@@ -425,28 +425,28 @@ Cpu.prototype = {
       this._pc = this._pop();
     }
   },
-  
+
   // RET p
   opf0() {
     this._log('ret p');
     this._ret(!this._getFlag(FLAG_N));
   },
-  
+
   // RET nz
   opc0() {
     this._log('ret nz');
     this._ret(!this._getFlag(FLAG_Z));
   },
-  
+
   opc8() {
     this._log('ret z');
     this._ret(!!this._getFlag(FLAG_Z));
   },
-  
+
   /**
    * CP
    */
-  
+
   // CP x
   opfe() {
     const op = this._readOp();
@@ -455,7 +455,7 @@ Cpu.prototype = {
     this._setFlags(value);
     this._cycles += 7;
   },
-  
+
   /**
    * AND
    */
@@ -468,7 +468,7 @@ Cpu.prototype = {
     this._setFlag(FLAG_C, false);
     this._cycles += 4;
   },
-  
+
   opa7() {
     this._and(this[REG_A]);
   }
